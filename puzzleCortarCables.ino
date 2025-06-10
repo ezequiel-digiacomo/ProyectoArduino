14940bd0adf897435ccfdf3d6ee79689bc96ee0f
Augusto Zimmermann <afzimmermann@unsam-bue.edu.ar>
2025-06-10 15:59:55 -0300

    Numeros random y multiples casos
    Agregado un generador de numeros casi random y 4 casos distintos de desactivación fallida

--- puzzleCortarCables.ino
+++ puzzleCortarCables.ino
@@ -1,5 +1,7 @@
 // https://www.tinkercad.com/things/eyVrCm9FVkO-cortar-cables?sharecode=5TrbO6dEMBiWb5AyjUhtVOFcYELgAcqe-cOzigVF5PI
 
+long valorRandom;
+
 void setup() {
   // Conexion serial
   Serial.begin(9600);
@@ -13,25 +15,61 @@ void setup() {
 
   // Si la bomba explota, se prende el led integrado
   pinMode(13, OUTPUT);
+
+  // https://docs.arduino.cc/language-reference/en/functions/random-numbers/random/
+  randomSeed(analogRead(0)); // Si el pin analógico 0 está desconectado, el ruido hace que randomSeed() genere números random
+  valorRandom = random(4); // Valor random entre 0 y 3
+  
+  // Muestro el valor random en el serial
+  Serial.println(valorRandom);
+
   delay(1000);  // Podria ser activada con un sensor que detecte cuando se mueve
 }
 
 void loop() {
   // Se pone en una variable la lectura digital
-  int cable1 = digitalRead(2);
-  int cable2 = digitalRead(3);
-  int cable3 = digitalRead(4);
-  int cable4 = digitalRead(5);
-
-  // Muestro el valor en el serial
-  Serial.println(cable3);
-
-  // Si se corta el cable 3 explota la bomba
-  if (cable3 == HIGH) {
-    digitalWrite(13, HIGH);
-    Serial.write("fiumba");
-    while (true) {
-      /* nada */;
-    }
+  int cable0 = digitalRead(2);
+  int cable1 = digitalRead(3);
+  int cable2 = digitalRead(4);
+  int cable3 = digitalRead(5);
+  
+  // Diferentes casos
+  switch (valorRandom) {
+    case 0:
+      if (cable0 == HIGH) {
+        digitalWrite(13, HIGH);
+        Serial.write("fiumba");
+        while (true) {
+          /* nada */;
+        }
+      }
+      break;
+    case 1:
+      if (cable1 == HIGH) {
+        digitalWrite(13, HIGH);
+        Serial.write("fiumba");
+        while (true) {
+          /* nada */;
+        }
+      }
+      break;
+    case 2:
+      if (cable2 == HIGH) {
+        digitalWrite(13, HIGH);
+        Serial.write("fiumba");
+        while (true) {
+          /* nada */;
+        }
+      }
+      break;
+    case 3:
+      if (cable3 == HIGH) {
+        digitalWrite(13, HIGH);
+        Serial.write("fiumba");
+        while (true) {
+          /* nada */;
+        }
+      }
+      break;
   }
 }
