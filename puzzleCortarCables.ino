e7694977ee441e141a0a17a427fb5b92ce03748e
Augusto Zimmermann <afzimmermann@unsam-bue.edu.ar>
2025-06-22 18:21:27 -0300

    LCD I2C añadido
    Ahora el LCD muestra la info necesaria, en vez del serial

--- puzzleCortarCables.ino
+++ puzzleCortarCables.ino
@@ -1,10 +1,15 @@
 // https://wokwi.com/projects/434416853604919297
 
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
 #define CABLE_0 3
 #define CABLE_1 4
 #define CABLE_2 5
 #define CABLE_3 6
 #define LED_PIN 13
+
+LiquidCrystal_I2C digitalLCD(0x27, 16, 2);
 
 long valorRandom;
 int i = 0;
@@ -18,9 +23,17 @@ int ordenCorrecto[4][4] = {
 int pasoActual = 0;
 
 void setup() {
+  // Inicializo el LCD
+  digitalLCD.init();
+  digitalLCD.init();
+  digitalLCD.backlight();
+  digitalLCD.clear();
+  
   // Conexion serial
   Serial.begin(9600);
-  Serial.println("Armada");
+  
+  digitalLCD.setCursor(0,0);
+  digitalLCD.print("Armada");
 
   // Configuracion de pines
   pinMode(CABLE_0, INPUT_PULLUP);
@@ -35,9 +48,13 @@ void setup() {
   randomSeed(analogRead(0)); // Si el pin analógico 0 está desconectado, el ruido hace que randomSeed() genere números random
   valorRandom = random(4); // Valor random entre 0 y 3
   
-  // Muestro el valor random en el serial
-  Serial.print("El codigo es: ");
-  Serial.println(valorRandom);
+  // Muestro el valor random en el LCD
+  digitalLCD.setCursor(0,1);
+  digitalLCD.print("El codigo es: ");
+  digitalLCD.setCursor(14,1);
+  digitalLCD.print(valorRandom);
+  //Serial.print("El codigo es: ");
+  //Serial.println(valorRandom);
 }
 
 void loop() {
@@ -50,7 +67,10 @@ void loop() {
     pasoActual++; // Actualizo el contador
 
     if (pasoActual == 4) {
-      Serial.println("Bomba desactivada!");
+      digitalLCD.clear();
+      digitalLCD.setCursor(0,0);
+      digitalLCD.print("Desactivada!");
+      // Serial.println("Bomba desactivada!");
       while (true) {/* nada */}
     }
   }
@@ -59,7 +79,10 @@ void loop() {
   for (int i = 0; i < 4; i++) {
     int pin = ordenCorrecto[valorRandom][i];
     if (i != pasoActual && !cableCortado[i] && digitalRead(pin) == HIGH) {
-    	Serial.println("nt"); // Mensaje cuando explota
+    	digitalLCD.clear();
+      digitalLCD.setCursor(0,0);
+      digitalLCD.print("nt"); // Mensaje cuando explota
+      //Serial.println("nt"); // Mensaje cuando explota
   		digitalWrite(LED_PIN, HIGH);
   		while (true) {/* nada */}
     }
